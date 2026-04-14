ddbf667334821ad6959eeac90ad1c4a946ee0460
riddhij <riddhijathar7@gmail.com>
2026-04-15 01:30:26 +0530

    changed name chinmay - riddhi

--- data.js
+++ data.js
@@ -12,9 +12,9 @@ const portfolioData = {
   // ----------------------------------------------------------
   // 🧑 PERSONAL INFO
   // ----------------------------------------------------------
-  name: "Chinmay Ingle",
-  firstName: "Chinmay",   // Used in the hero greeting
-  lastName: "Ingle",      // Displayed large in the hero section
+  name: "Riddhi Jathar",
+  firstName: "Riddhi",    // Used in the hero greeting
+  lastName: "Jathar",      // Displayed large in the hero section
   tagline: "Engineering Student & Aspiring Developer",
   resumeLink: "#",        // Link to your resume (Google Drive, etc.)
 
